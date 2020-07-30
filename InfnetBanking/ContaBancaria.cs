f3372e533bd8d744413991f5372a6d1f4d3c3c6f
pl-silva <paulinhoow@hotmail.com.br>
2020-07-30 17:53:56 -0300

    Adicionar arquivos de projeto.

---
+++ InfnetBanking/ContaBancaria.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfnetBanking
+{
+    public class ContaBancaria
+    {
+        // Classes possuem membros.
+        // Membros podem ser campos/propriedades ou métodos.
+        private double saldo; // campo privado: apenas a própria classe consegue acessar.
+
+        // Propriedade (C#)
+        // Propriedades serão, em geral, públicas.
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+        }
+
+        // Poderíamos ter criado Saldo com uma autopropriedade apenas com get.
+        // public double Saldo { get; }
+
+        // Autopropriedade (C# 3.0 ou superior)
+        // Padrão da linguagem: nomes de campos com minúscula; nomes de propriedades com maiúscula
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+
+        // E para escrever o saldo?
+        // Vamos utilizar métodos que contenham as regras de negócio para manipular o saldo.
+
+        // Os métodos, em geral, serão públicos (Haverá exceções)
+        public void Depositar(double valor)
+        {
+            if (valor >= 0)
+            {
+                saldo += valor;
+            }
+            else
+            {
+                Console.WriteLine("Não é possível depositar valores engativos.");
+            }
+        }
+
+        public void Sacar(double valor)
+        {
+            if (saldo >= valor)
+            {
+                saldo -= valor;
+                Console.WriteLine($"Saque finalizado. O novo saldo é de R${saldo}.");
+            }
+            else
+            {
+                Console.WriteLine("Não foi possível efetuar o saque. Seu saldo é insuficiente.");
+            }
+        }
+
+        public void Transferir(double valor, ContaBancaria conta)
+        {
+            if (valor >= 0 && valor <= saldo)
+            {
+                Sacar(valor);
+                conta.Depositar(valor);
+            }
+        }
+    }
+}
---
+++ InfnetBanking/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace InfnetBanking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Instanciar objeto do tipo ContaBancaria
+            ContaBancaria contaBancaria = new ContaBancaria(); // Construtor padrão da classe
+
+            // O construtor é uma estrutura presente em todas as classes e que sabe instanciar objetos da classe.
+            // O construtor chamado "padrão" não recebe parâmetro e pode ficar "invisível" na classe.
+
+            // 2. Acessar (ler e/ou escrever) propriedades:
+            contaBancaria.Agencia = 3652;
+            contaBancaria.Numero = 30680;
+            contaBancaria.Titular = "Agenor Leopoldo Fagundes";
+            Console.WriteLine($"Titular: {contaBancaria.Titular}.");
+            Console.WriteLine($"Agência: {contaBancaria.Agencia}.");
+            Console.WriteLine($"Conta: {contaBancaria.Numero}.");
+            Console.WriteLine($"Saldo inicial: {contaBancaria.Saldo}.");
+
+            Console.Write("Informe o valor do depósito inicial: R$ ");
+            double valor = Double.Parse(Console.ReadLine());
+            contaBancaria.Depositar(valor);
+
+
+            Console.WriteLine($"Saldo: {contaBancaria.Saldo}.");
+            // Não pode:
+            // contaBancaria.Saldo = 1000; // <- Porque Saldo  é uma propriedade somente leitura
+            // Para "escrever" o saldo, usaremos  métodos Sacar() e Depositar().
+
+            Console.Write($"Informe o valor de saque: R$");
+            valor = Double.Parse(Console.ReadLine());
+            contaBancaria.Sacar(valor);
+
+            ContaBancaria contaBancaria2 = new ContaBancaria();
+            contaBancaria2.Depositar(100);
+
+            contaBancaria.Transferir(100, contaBancaria2);
+            Console.WriteLine($"Saldo da primeira conta: {contaBancaria.Saldo}");
+            Console.WriteLine($"Saldo da segunda conta: {contaBancaria2.Saldo}");
+
+        }
+    }
+}
